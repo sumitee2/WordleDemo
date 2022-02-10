508188d07f619af868925713f457463d1cfde840
sumitchaudhary <sumit.einstein@gmail.com>
2022-02-10 21:07:12 +0530

    Initial Commit

---
+++ WordleDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordleDemo
+//
+//  Created by Sumit Chaudhary on 10/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordleDemo/WordleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleDemoApp.swift
+//  WordleDemo
+//
+//  Created by Sumit Chaudhary on 10/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
